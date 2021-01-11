4dea2023b9feaa9d44645111c7a2248b512d651f
Sergio Zanchetta <primes2h@gmail.com>
2021-01-11 21:09:27 +0100

    [ADD] new module account_receipt_print

---
+++ account_receipt_print/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Receipt Printing",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "summary": "Enable printing in sale and purchase receipts",
+    "author": "Sergio Zanchetta - Associazione PNLug APS, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_view.xml",
+        "views/report_receipt.xml",
+        "views/account_report.xml",
+    ],
+    "installable": True,
+}
